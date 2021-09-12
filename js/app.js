d1ffed0ee4b9220714d96fa6fb63d85eb0091948
shajjadhossanWD <shajjadhossan111@gmail.com>
2021-09-13 03:32:36 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h4><i class="fas fa-star rating"></i> ${product.rating.rate} (${product.rating.count} peoples)</h4>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
